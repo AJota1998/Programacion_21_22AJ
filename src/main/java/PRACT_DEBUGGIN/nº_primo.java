c968fac3c6989e19c03f663f28d538e3a7aa0126
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2021-10-27 18:09:18 +0200

    añado tarea

---
+++ src/main/java/PRACT_DEBUGGIN/nº_primo.java
@@ -0,0 +1,34 @@
+package PRACT_DEBUGGIN;
+
+import java.util.Scanner;
+
+public class nº_primo {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el núemero");
+        num = sc.nextInt();
+
+        if (esPrimo(num)) {
+            System.out.println("El número " + num + " es primo");
+        } else {
+            System.out.println("El número " + num + " no es primo");
+        }
+    }
+
+    public static boolean esPrimo(int num) {
+
+        boolean esPrimo = true;
+        for (int i = 2; i < num; i++) { //con este for se determina si el número es primo o no
+            if (num % i == 0) {
+                esPrimo = false;
+                break;
+            } else {
+                esPrimo = true;
+            }
+        }
+        return esPrimo;
+    }
+
+}
