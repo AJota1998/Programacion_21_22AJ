b7e195837a15910e9782478fd432231744a2f0de
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2021-11-02 15:30:44 +0100

    tarea

--- src/main/java/Practica_Entregable/digitos6cifras.java
+++ src/main/java/Practica_Entregable/digitos6cifras.java
@@ -12,32 +12,51 @@ public class digitos6cifras {
         System.out.println("Introduce un número de 6 cifras");
         int numero = teclado.nextInt();
 
-            while (numero < 100000 || numero > 999999) {
-                System.out.println("Introduce un número válido");
-                numero = teclado.nextInt();
-            }
+        while (numero < 100000 || numero > 999999) {
+            System.out.println("Introduce un número válido");
+            numero = teclado.nextInt();
+        }
 
-            int aux;
-            int cifra = 0;
-            int inverso = 0;
+        int aux;
+        int cifra = 0;
+        int inverso = 0;
 
-            aux = numero;
-            int variable = 0;
-            int contador = 0;
-                                        //esto coge cada cifra de un número
-            while (contador < 3) {
-                    cifra = aux % 10;
-                    inverso = (inverso * 10) + cifra;
-                    aux = aux / 10;
-                    contador++;
-            }
+        aux = numero;
+        int variable = 0;
+        int contador = 0;
+        //esto coge cada cifra de un número
+        while (contador < 3) {
+            cifra = aux % 10;
+            inverso = (inverso * 10) + cifra;
+            aux = aux / 10;
+            contador++;
+        }
         System.out.println(aux);
         System.out.println(inverso);
 
-            //ahora lo termino, tengo que darle la vuelta al 654
+        int aux2;
+        int cifra2 = 0;
+        int inverso2 = 0;
 
+        while (inverso != 0) {
+            cifra2 = inverso % 10;
+            inverso2 = (inverso2 * 10) + cifra2;
+            inverso = inverso / 10;
+        }
+        System.out.println(inverso2);
 
+        boolean esprimo = true;
+        for (int i = 2; i < aux; i++) {
+            if (aux % i == 0) {
+                esprimo = false;
+                break;
+            }
 
-
+        }
+        if (esprimo) {
+            System.out.println("el número " + aux + " es primo ");
+        } else {
+            System.out.println("el número " + aux + " no es primo");
+        }
     }
 }
