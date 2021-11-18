5c994937d6c5b72be36a81fb6a6c236863a140a7
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2021-11-18 18:00:33 +0100

    tarea

--- src/main/java/TEMA3/arrays/array4.java
+++ src/main/java/TEMA3/arrays/array4.java
@@ -1,5 +1,6 @@
 package TEMA3.arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class array4 {
@@ -7,9 +8,52 @@ public class array4 {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduce una longitud de dígitos de clave");
-        int digi = teclado.nextInt();
-                                                        //meter la clave en un array
-        double combinacion = (1 + Math.random() * 6);
-        System.out.println(combinacion);
+        int l = teclado.nextInt();
+
+        int[] clave = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            clave[i] = (int) (1 + (Math.random() * 6));
+        }
+
+        int[] prueba = new int[l];
+
+
+        for (int i = 0; i < l; i++) {
+            System.out.println("Introduce un numero entre 1 y 5");
+            prueba[i] = teclado.nextInt();
+        }
+
+        System.out.println("Intente acertarlo");
+
+        for (int i = 0; i < l; i++) {
+            if (clave[i] < prueba[i]) {
+                System.out.println("Es menor");
+            } else if (clave[i] > prueba[i]) {
+                System.out.println("Es mayor");
+            } else {
+                System.out.println("Es igual");
+            }
+        }
+
+        while (!Arrays.equals(clave, prueba)) {
+            System.out.println("Inténtelo de nuevo");
+            for (int i = 0; i < l; i++) {
+                System.out.println("Introduce un número entre 1 y 5");
+                prueba[i] = teclado.nextInt();
+            }
+            for (int i = 0; i < l; i++) {
+                if (clave[i] < prueba[i]) {
+                    System.out.println("Es menor");
+                } else if (clave[i] > prueba[i]) {
+                    System.out.println("Es mayor");
+                } else {
+                    System.out.println("Es igual");
+                }
+            }
+
+        }
     }
 }
+
+
---
+++ src/main/java/TEMA3/arrays/array5.java
@@ -0,0 +1,10 @@
+package TEMA3.arrays;
+
+import java.util.Scanner;
+
+public class array5 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+    }
+}
