622cf350fb0acb796056e818999810489c94d1a3
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2021-12-01 18:05:45 +0100

    tarea

--- src/main/java/INTERNET/ej12.java
+++ src/main/java/INTERNET/ej12.java
@@ -9,15 +9,14 @@ public class ej12 {
         int []divi = new int[0];
 
         for (int i = 1; i < 101; i++) {
-            System.out.println(i);
+            System.out.print(i + " ");
             if (i % 2 == 0 && i % 3 == 0) {
-                divisibles++;
                 divi = Arrays.copyOf(divi, divi.length + 1);
-                divi[divisibles] = i;
+                divi[divisibles++] = i; //Al poner divisibles++ nos ahorramos una linea para incremetar el contador.
             }
         }
-        System.out.println("El número total de numeros divisibles entre 2 y 3 es " + divisibles);
+        System.out.println("El total de numeros divisibles entre 2 y 3 es " + divisibles);
         System.out.println();
-        System.out.print(Arrays.toString(divi));
+        System.out.print("Números: " + Arrays.toString(divi));
     }
 }
