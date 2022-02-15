bb93e05016c73a3e74582f5a84dc1c6d874a091c
Ajota1998 <alum.aortizg.2021@iesalixar.org>
2022-02-15 12:33:47 +0100

    tarea

---
+++ src/main/java/YOUTUBE/ABSTRACT/SerVivo/AnimalHerbivoro.java
@@ -0,0 +1,9 @@
+package YOUTUBE.ABSTRACT.SerVivo;
+
+public class AnimalHerbivoro extends Animal {
+
+    @Override
+    public void alimentarse() {
+        System.out.println("El animal herbivoro se alimenta de hierba");
+    }
+}
