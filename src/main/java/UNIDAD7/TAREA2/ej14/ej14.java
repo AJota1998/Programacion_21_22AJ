9e6236609e2ab4840fec969c30bc66b77ac05216
ajota1998 <alum.aortizg.2021@ieslixar.org>
2022-03-16 18:07:28 +0100

    tarea

---
+++ src/main/java/UNIDAD7/TAREA2/ej14/ej14.java
@@ -0,0 +1,29 @@
+package UNIDAD7.TAREA2.ej14;
+
+/* Implementar la función leeCadena con el siguiente prototipo:
+        List<Character> leeCadena()
+Dicha función lee una cadena por teclado y nos la devuelve en una lista con un carácter en cada nodo. */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ej14 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca una frase pequeña");
+        String cadena = teclado.nextLine();
+
+        System.out.println(cadena);
+        System.out.println(lea_cadena(cadena));
+
+    }
+    public static List<Character> lea_cadena(String cad) {
+        ArrayList<Character> cadenita = new ArrayList<>();
+        for (int i = 0; i < cad.length(); i++) {
+            cadenita.add(cad.charAt(i));
+        }
+        return cadenita;
+    }
+}
---
+++ src/main/java/UNIDAD7/TAREA3B/ej1Yej2.java
@@ -0,0 +1,61 @@
+package UNIDAD7.TAREA3B;
+
+/*
+1- Crea un HashMap cuya clave sea un Integer, y cuyos valores sean los nombres de algunos de tus compañeros.
+La clave ha de calcularse mediante un método que reciba un String que represente el dni,
+y devuelva la suma de sus dígitos.
+46221877D -> 4+6+2+2+1+8+7+7=37
+Una vez guardados los elementos, deben mostrarse todos los elementos (clave-valor) del HasMap.
+
+2- Añada al programa anterior la siguiente funcionalidad.
+El programa pedirá al usuario un dni, calculará la clave que ha de consultar,
+comprobará que dicha clave existe, y, en tal caso, mostrará el valor asociado a esa clave en el HashMap.
+*/
+import java.util.*;
+
+public class ej1Yej2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        Map<Integer, String> mapa = new HashMap<>();
+
+        mapa.put(suma_dni("17845678"), "Adrian");
+        mapa.put(suma_dni("98765332"), "Ivan");
+        mapa.put(suma_dni("26491120"), "Alfonso");
+        mapa.put(suma_dni("23458754"), "Juan Pablo");
+        mapa.put(suma_dni("88893411"), "Joserra");
+
+        //imprimimos el valor que tiene asociado la clave
+        System.out.println(mapa.get(suma_dni("98765332")));
+
+
+        //manera de imprimir el valor que tiene la propia clave
+        Set<Integer> claves = mapa.keySet();
+
+        Iterator<Integer> it = claves.iterator();
+        while (it.hasNext()) {
+            Integer key = it.next();
+            System.out.println(key);
+        }
+
+        System.out.println("Introduzca un dni");
+        String dni_usuario = teclado.next();
+        int consultar = suma_dni(dni_usuario);
+
+        // Para saber si una clave está almacenada en un mapa
+        boolean tiene_clave = mapa.containsKey(suma_dni(dni_usuario));
+        System.out.println("La clave " + consultar + ", ¿está en el mapa?, " + tiene_clave);
+
+        if(tiene_clave) {
+            System.out.println(mapa.get(suma_dni(dni_usuario)));
+        }
+
+    }
+    public static int suma_dni(String dni) {
+        int suma = 0;
+        for (int i = 0; i < dni.length(); i++) {
+            suma += Integer.parseInt(String.valueOf(dni.charAt(i)));
+        }
+        return suma;
+    }
+}
